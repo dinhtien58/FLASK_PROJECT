13ca3861df3139e5a7259da1690899b413e59731
ĐinhTiến58 <92936472+dinhtien58@users.noreply.github.com>
2023-04-19 15:30:45 +0700

    Update sql_alchemy.py

--- Tutorial_Flask/sql_alchemy.py
+++ Tutorial_Flask/sql_alchemy.py
@@ -6,7 +6,7 @@ from flask_sqlalchemy import SQLAlchemy
 from os import path
 
 app = Flask(__name__)
-app.config["SECRET_KEY"] = "thanhdz"
+app.config["SECRET_KEY"] = "dinhtien"
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///user.db"
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 app.permanent_session_lifetime = timedelta(minutes=1)
